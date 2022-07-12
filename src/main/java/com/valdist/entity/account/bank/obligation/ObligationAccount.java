ed24da879af19bc16f718bede50c51cab784b11b
valdis-t <vladislavkharkov95@gmail.com>
2022-07-12 22:54:03 +0300

    create abstract class ObligationAccount for featured accounts which increase their balances

---
+++ src/main/java/com/valdist/entity/account/bank/obligation/ObligationAccount.java
@@ -0,0 +1,16 @@
+package com.valdist.entity.account.bank.obligation;
+
+import com.valdist.entity.account.Account;
+
+public abstract class ObligationAccount extends Account {
+    private long interestRate;
+    private Account accountForPayingObligation;
+
+    protected ObligationAccount(long accountNumber, String name, long balance, String currency) {
+        super(accountNumber, name, balance, currency);
+    }
+
+    public void setAccountForPayingObligation(Account account) {
+        this.accountForPayingObligation = accountForPayingObligation;
+    }
+}
