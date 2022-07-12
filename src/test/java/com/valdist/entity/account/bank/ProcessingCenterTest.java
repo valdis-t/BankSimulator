cd448e6972617b8d09937ebcea56ea6be0725c16
valdis-t <vladislavkharkov95@gmail.com>
2022-07-13 02:09:53 +0300

    replace switch-case by CommandLineUI.execution()

--- src/test/java/com/valdist/entity/account/bank/ProcessingCenterTest.java
+++ src/test/java/com/valdist/entity/account/bank/ProcessingCenterTest.java
@@ -4,59 +4,41 @@ import com.valdist.entity.account.Account;
 import com.valdist.exception.processing.AccountHasNotFoundException;
 import com.valdist.processing.ProcessingCenter;
 import com.valdist.util.commandline.CommandLineUI;
+import com.valdist.util.commandline.Execution;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 
 public class ProcessingCenterTest {
     public static void main(String[] args) {
         CommandLineUI ui = new CommandLineUI();
         Collection<Account> accounts = new HashSet<>();
-        ProcessingCenter processingCenter;
         ui.println("Processing Center Test");
 
-        while (true) {
-            while (true) {
+        do {
+            do {
                 accounts.add(ui.getRawAccount());
-                if (!ui.getBoolean("Add new one account?", "y")) break;
-            }
-            processingCenter = new ProcessingCenter(accounts);
+            } while (ui.getBoolean("Add new one account?", "y"));
 
-            String menu = "1 - make transaction\n" +
-                    "2 - get account info\n" +
-                    "3 - set account\n" +
-                    "0 - exit from menu\n" +
-                    "Input";
-            label:
-            {
-                while (true) {
-                    switch (ui.getInt(menu)) {
-                        case 1: {
-                            processingCenter.makeTransaction(ui.getRawTransaction());
-                            break;
-                        }
-                        case 3: {
-                            ui.println("Accounts");
-                            processingCenter.getAccountNumbers().forEach(ui::println);
-                            try {
-                                processingCenter.setCurrentAccount(ui.getLong("Input account"));
-                            } catch (AccountHasNotFoundException e) {
-                                ui.println(e.getMessage());
-                            }
-                            break;
-                        }
-                        case 2: {
-                            ui.println(processingCenter.getAccountToString());
-                            break;
-                        }
-                        case 0: {
-                            break label;
-                        }
-                    }
-                }
-            }
-            if (ui.getBoolean("Exit?", "y")) break;
-        }
+            ProcessingCenter processingCenter = new ProcessingCenter(accounts);
 
+            ui.execute(
+                    List.of(
+                            new Execution("1", "make transaction", () -> processingCenter.makeTransaction(ui.getRawTransaction())),
+                            new Execution("2", "get account info", () -> ui.println(processingCenter.getAccountToString())),
+                            new Execution("3", "set account", () -> {
+                                ui.println("Accounts");
+                                processingCenter.getAccountNumbers().forEach(ui::println);
+                                try {
+                                    processingCenter.setCurrentAccount(ui.getLong("Input account"));
+                                } catch (AccountHasNotFoundException e) {
+                                    ui.println(e.getMessage());
+                                }
+                            })),
+                    new Execution("0", "exit", () -> {
+                    }),
+                    true);
+        } while (ui.getBoolean("Reapeat the test?", "y"));
     }
 }
