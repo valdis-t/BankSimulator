9ea7248bd74684ebfc1e9644a5d364483b8cfe49
valdis-t <vladislavkharkov95@gmail.com>
2022-07-12 22:57:35 +0300

    create exception classes ProcessingCenterException, AccountHasNotFoundException for ProcessingCenter

---
+++ src/main/java/com/valdist/exception/processing/ProcessingCenterException.java
@@ -0,0 +1,7 @@
+package com.valdist.exception.processing;
+
+public class ProcessingCenterException extends Exception {
+    public ProcessingCenterException(String message) {
+        super(message);
+    }
+}
